fde480d9b48321a5fb291940852be84a95ebfffa
Lucas Desenvolvedor <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2021-11-30 22:09:01 -0300

    CSHARP - AULA01

---
+++ Aprendendo a programar com C#/aula_01/PrimeiroProjeto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;  //referência para um namespace chamado System.
+
+
+namespace PrimeiroProjeto  //mostra que a class program está dentro do namespace chamado Primeiro.
+
+{
+    //CLASS = unidade básica da programação orientada a objetos, o corpo da classe é delimitado por chaves {  }.
+
+
+    class Program
+    {
+        static void Main(string[] args) //declaração padrão, para indicar onde a execução vai começar.
+        {
+            //Grava a representação de texto dos objetos especificados
+            Console.WriteLine("Hello World!");     
+        }
+
+        /*
+            Guia de Referência
+
+            Mudar o idioma
+            Ferramentas > Opções > Ambiente > Configurações Internacionais > Idioma
+            Tools > Options > Environment > International Settings > Language
+
+            Como fechar e reabrir o projeto?
+                 -  Abra o arquivo.sln
+                 -  Indentação automática: CTRL + K + D
+            Tamanho da Fonte: CTRL + Mouse Scroll
+        */
+    }
+}
