52ddb9fb2fcf231b7b26c7570a1a58e9d373c4b5
Lucas Desenvolvedor <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2021-12-18 00:20:16 -0300

    aula04 api

---
+++ Microsoft Platform e Projeto final/aula04/jornada_ejogos_api_/Controllers/EquipeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jornada_ejogos_api_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult localizar()
+        {
+
+            return Ok("Localizar as equipes");
+
+        }
+
+    }
+}
