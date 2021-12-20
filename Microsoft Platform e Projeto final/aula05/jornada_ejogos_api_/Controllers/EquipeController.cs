da2c1a62a277650361f385ca3a14317254747943
Lucas Desenvolvedor <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2021-12-20 19:52:33 -0300

    aula05

---
+++ Microsoft Platform e Projeto final/aula05/jornada_ejogos_api_/Controllers/EquipeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jornada_ejogos_api_.Controllers
+{
+    //padrão de rota, DOMINIO/api/nomecontroller
+    [Route("api/[controller]")]
+    [ApiController]
+
+                                    //herança ControllerBase funções ja prontas.
+    public class EquipeController : ControllerBase
+    {
+        //VERBO (estará disponivel para a consulta.
+        [HttpGet]
+        public IActionResult qualquercoisa()
+        {
+            return Ok("Localizar equipes");
+        }
+
+
+
+
+    }
+}
